275a598bd594d7b23430abb04f5ed68d4a94c9ed
John McDonnell <john_mcdonnell@hotmail.com>
2016-03-13 17:13:30 +0000

    Formatting.

--- MockTCPServer/src/main/java/io/cloudracer/datastream/DataStream.java
+++ MockTCPServer/src/main/java/io/cloudracer/datastream/DataStream.java
@@ -13,294 +13,279 @@ import org.apache.log4j.Logger;
 
 public class DataStream {
 
-	private Logger logger;
+    private Logger logger;
 
-	public final static int TAIL_LENGTH_DEFAULT = 3;
+    public final static int TAIL_LENGTH_DEFAULT = 3;
 
-	private ByteArrayOutputStream output;
-	private Deque<Byte> tailQueue = null;
-	private int lastByte;
-	private Integer tailLength = null;
-	private String rootLoggerName;
+    private ByteArrayOutputStream output;
+    private Deque<Byte> tailQueue = null;
+    private int lastByte;
+    private Integer tailLength = null;
+    private String rootLoggerName;
 
-	/**
-	 * Specify a log4j root logger.
-	 *
-	 * @param rootLoggerName
-	 *            log4j root logger
-	 */
-	public DataStream(final String rootLoggerName) {
-		setTailLength(TAIL_LENGTH_DEFAULT);
-		setRootLoggerName(rootLoggerName);
-	}
+    /**
+     * Specify a log4j root logger.
+     *
+     * @param rootLoggerName log4j root logger
+     */
+    public DataStream(final String rootLoggerName) {
+        setTailLength(TAIL_LENGTH_DEFAULT);
+        setRootLoggerName(rootLoggerName);
+    }
 
-	/**
-	 * Specify a {@link DataStream#getRootLoggerName() log4j root logger} and
-	 * the {@link DataStream#getTailLength() maximum length of the stream tail}.
-	 *
-	 * @param rootLoggerName
-	 *            log4j root logger. Default to the
-	 * @param tailLength
-	 *            tail length. Default of {@link DataStream#TAIL_LENGTH_DEFAULT}
-	 */
-	public DataStream(final String rootLoggerName, final int tailLength) {
-		setTailLength(tailLength);
-		setRootLoggerName(rootLoggerName);
-	}
+    /**
+     * Specify a {@link DataStream#getRootLoggerName() log4j root logger} and the {@link DataStream#getTailLength() maximum length of the stream tail}.
+     *
+     * @param rootLoggerName log4j root logger. Default to the
+     * @param tailLength tail length. Default of {@link DataStream#TAIL_LENGTH_DEFAULT}
+     */
+    public DataStream(final String rootLoggerName, final int tailLength) {
+        setTailLength(tailLength);
+        setRootLoggerName(rootLoggerName);
+    }
 
-	/**
-	 * Delegate of {@link PipedOutputStream#write(int)}.
-	 *
-	 * @param data
-	 *            data to write to the {@link PipedInputStream}.
-	 * @throws IOException
-	 */
-	public synchronized void write(final int data) throws IOException {
-		getOutput().write(data);
-	}
+    /**
+     * Delegate of {@link PipedOutputStream#write(int)}.
+     *
+     * @param data data to write to the {@link PipedInputStream}.
+     * @throws IOException
+     */
+    public synchronized void write(final int data) throws IOException {
+        getOutput().write(data);
+    }
 
-	/**
-	 * {@link PipedOutputStream#close() close} the {@link PipedOutputStream
-	 * output stream}.
-	 *
-	 * @throws IOException
-	 */
-	public void close() throws IOException {
-		setOutput(null);
-	}
+    /**
+     * {@link PipedOutputStream#close() close} the {@link PipedOutputStream output stream}.
+     *
+     * @throws IOException
+     */
+    public void close() throws IOException {
+        setOutput(null);
+    }
 
-	/**
-	 * Delegate of {@link ByteArrayOutputStream#size()}.
-	 */
-	public synchronized int size() {
-		return getOutput().size();
-	}
+    /**
+     * Delegate of {@link ByteArrayOutputStream#size()}.
+     */
+    public synchronized int size() {
+        return getOutput().size();
+    }
 
-	public synchronized ByteArrayOutputStream getOutput() {
-		if (output == null) {
-			output = new ByteArrayOutputStream() {
-				@Override
-				public synchronized void write(int b) {
-					super.write(b);
-					setLastByte(b);
-					addToTailList();
-				}
-			};
-		}
+    public synchronized ByteArrayOutputStream getOutput() {
+        if (output == null) {
+            output = new ByteArrayOutputStream() {
+                @Override
+                public synchronized void write(int b) {
+                    super.write(b);
+                    setLastByte(b);
+                    addToTailList();
+                }
+            };
+        }
 
-		return output;
-	}
+        return output;
+    }
 
-	/**
-	 * Copies the stream to a newly created {@link PipedInputStream input
-	 * stream}. The {@link PipedInputStream input stream} is cached internally
-	 * and only reinitialised when new bytes are written to the stream.
-	 * <p>
-	 * The stream is copied without making one or more intermediary copies of
-	 * the content (i.e. it is memory efficient).
-	 *
-	 * @return {@link PipedInputStream input stream} populated with all
-	 *         available data.
-	 * @throws IOException
-	 */
-	public PipedInputStream copyToInputStream() throws IOException {
-		PipedInputStream inputStream = new PipedInputStream(getOutput().size());
-		final PipedOutputStream outputStream = new PipedOutputStream(inputStream);
+    /**
+     * Copies the stream to a newly created {@link PipedInputStream input stream}. The {@link PipedInputStream input stream} is cached internally and only reinitialised when new bytes are written to the stream.
+     * <p>
+     * The stream is copied without making one or more intermediary copies of the content (i.e. it is memory efficient).
+     *
+     * @return {@link PipedInputStream input stream} populated with all available data.
+     * @throws IOException
+     */
+    public PipedInputStream copyToInputStream() throws IOException {
+        PipedInputStream inputStream = new PipedInputStream(getOutput().size());
+        final PipedOutputStream outputStream = new PipedOutputStream(inputStream);
 
-		Thread copy = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					getOutput().writeTo(outputStream);
-				} catch (IOException e) {
-					logger.error(e.getMessage(), e);
-				}
-			}
-		});
-		copy.setName(String.format("%s-InputStream-Creator", getThreadName()));
-		copy.start();
-		try {
-			copy.join();
-		} catch (InterruptedException e) {
-			logger.error(e.getCause(), e);
-		}
+        Thread copy = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    getOutput().writeTo(outputStream);
+                } catch (IOException e) {
+                    logger.error(e.getMessage(), e);
+                }
+            }
+        });
+        copy.setName(String.format("%s-InputStream-Creator", getThreadName()));
+        copy.start();
+        try {
+            copy.join();
+        } catch (InterruptedException e) {
+            logger.error(e.getCause(), e);
+        }
 
-		return inputStream;
-	}
+        return inputStream;
+    }
 
-	/**
-	 * Instantiate the {@link ByteArrayOutputStream output stream}.
-	 *
-	 * @param outputStream
-	 *            if null, the current {@link PipedOutputStream output stream}
-	 *            is {@link PipedOutputStream#close() closed} before being
-	 *            reinitialised.
-	 * @throws IOException
-	 */
-	private void setOutput(final ByteArrayOutputStream output) throws IOException {
-		if (output == null && this.output != null) {
-			this.output.close();
-		}
+    /**
+     * {@link ByteArrayOutputStream#close() close} the {@link ByteArrayOutputStream output stream}.
+     *
+     * @param output if null, the current {@link ByteArrayOutputStream output stream} is closed before being reinitialised.
+     * @throws IOException
+     */
+    private void setOutput(final ByteArrayOutputStream output) throws IOException {
+        if (output == null && this.output != null) {
+            this.output.close();
+        }
 
-		this.output = output;
+        this.output = output;
 
-	}
+    }
 
-	/**
-	 * The length of the tail.
-	 *
-	 * @return the length of the tail. Default of
-	 *         {@link DataStream#TAIL_LENGTH_DEFAULT}.
-	 */
-	public int getTailLength() {
-		return tailLength;
-	}
+    /**
+     * The length of the tail.
+     *
+     * @return the length of the tail. Default of {@link DataStream#TAIL_LENGTH_DEFAULT}.
+     */
+    public int getTailLength() {
+        return tailLength;
+    }
 
-	private void setTailLength(final int tailLength) {
-		this.tailLength = tailLength;
-	}
+    private void setTailLength(final int tailLength) {
+        this.tailLength = tailLength;
+    }
 
-	/**
-	 * The tail of the specified {@link DataStream#getTailLength() length}.
-	 *
-	 * @return
-	 */
-	private Deque<Byte> getTailQueue() {
-		if (tailQueue == null) {
-			tailQueue = new ArrayDeque<Byte>(getTailLength());
-		}
+    /**
+     * The tail of the specified {@link DataStream#getTailLength() length}.
+     *
+     * @return
+     */
+    private Deque<Byte> getTailQueue() {
+        if (tailQueue == null) {
+            tailQueue = new ArrayDeque<Byte>(getTailLength());
+        }
 
-		return tailQueue;
-	}
+        return tailQueue;
+    }
 
-	/**
-	 * The tail of the specified {@link DataStream#getTailLength() length}.
-	 *
-	 * @return
-	 */
-	public byte[] getTail() {
-		// Convert the list to an array.
-		final byte[] tail = new byte[getTailQueue().size()];
-		int i = 0;
-		for (Iterator<Byte> iterator = getTailQueue().iterator(); iterator.hasNext();) {
-			final Byte nextByte = iterator.next();
-			tail[i] = nextByte;
+    /**
+     * The tail of the specified {@link DataStream#getTailLength() length}.
+     *
+     * @return a byte[] containing the tail data, of specified maximum length.
+     */
+    public byte[] getTail() {
+        // Convert the list to an array.
+        final byte[] tail = new byte[getTailQueue().size()];
+        int i = 0;
+        for (Iterator<Byte> iterator = getTailQueue().iterator(); iterator.hasNext();) {
+            final Byte nextByte = iterator.next();
+            tail[i] = nextByte;
 
-			i++;
-		}
+            i++;
+        }
 
-		return tail;
-	}
+        return tail;
+    }
 
-	private void addToTailList() {
-		getTailQueue().addLast(getLastByte());
-		if (getTailQueue().size() > getTailLength()) {
-			getTailQueue().removeFirst();
-		}
-	}
+    private void addToTailList() {
+        getTailQueue().addLast(getLastByte());
+        if (getTailQueue().size() > getTailLength()) {
+            getTailQueue().removeFirst();
+        }
+    }
 
-	/**
-	 * Get the most recently received byte of the stream.
-	 *
-	 * @return the first byte in the stream.
-	 */
-	public byte getLastByte() {
-		return (byte) lastByte;
-	}
+    /**
+     * Get the most recently received byte of the stream.
+     *
+     * @return the first byte in the stream.
+     */
+    public byte getLastByte() {
+        return (byte) lastByte;
+    }
 
-	private void setLastByte(final int lastByte) {
-		this.lastByte = lastByte;
-	}
+    private void setLastByte(final int lastByte) {
+        this.lastByte = lastByte;
+    }
 
-	/**
-	 * Obtain the log4j root logger name.
-	 *
-	 * @return String the log4j root logger name
-	 */
-	private String getRootLoggerName() {
-		return (new String(rootLoggerName));
-	}
+    /**
+     * Obtain the log4j root logger name.
+     *
+     * @return String the log4j root logger name
+     */
+    private String getRootLoggerName() {
+        return (new String(rootLoggerName));
+    }
 
-	/**
-	 * Set the log4j root logger name.
-	 *
-	 * @param rootLoggerName
-	 *            The name of the log4j root logger
-	 */
-	private void setRootLoggerName(String rootLoggerName) {
-		this.rootLoggerName = new String(rootLoggerName);
-		logger = Logger.getLogger(String.format("%s.%s", this.rootLoggerName, getClazzName()));
-	}
+    /**
+     * Set the log4j {@link Logger#getLogger root logger} name.
+     *
+     * @param rootLoggerName the name of the log4j root logger
+     */
+    private void setRootLoggerName(String rootLoggerName) {
+        this.rootLoggerName = new String(rootLoggerName);
+        logger = Logger.getLogger(String.format("%s.%s", this.rootLoggerName, getClazzName()));
+    }
 
-	private Logger getLogger() {
-		if (logger == null) {
-			logger = Logger.getLogger(String.format("%s.%s", getRootLoggerName(), getClazzName()));
-		}
+    private Logger getLogger() {
+        if (logger == null) {
+            logger = Logger.getLogger(String.format("%s.%s", getRootLoggerName(), getClazzName()));
+        }
 
-		return logger;
-	}
+        return logger;
+    }
 
-	private String getThreadName() {
-		final String delimeter = ".";
-		final String regEx = "\\.";
+    private String getThreadName() {
+        final String delimeter = ".";
+        final String regEx = "\\.";
 
-		String name = null;
+        String name = null;
 
-		if (this.getClass().getSimpleName() != null && this.getClass().getSimpleName().length() > 0) {
-			name = this.getClass().getSimpleName();
-		} else {
-			if (this.getClass().getName().contains(delimeter)) {
-				final String nameSegments[] = this.getClass().getName().split(regEx);
+        if (this.getClass().getSimpleName() != null && this.getClass().getSimpleName().length() > 0) {
+            name = this.getClass().getSimpleName();
+        } else {
+            if (this.getClass().getName().contains(delimeter)) {
+                final String nameSegments[] = this.getClass().getName().split(regEx);
 
-				name = String.format("%s-%s", this.getClass().getSuperclass().getSimpleName(),
-						nameSegments[nameSegments.length - 1]);
-			} else {
-				name = this.getClass().getName();
-			}
-		}
+                name = String.format("%s-%s", this.getClass().getSuperclass().getSimpleName(),
+                        nameSegments[nameSegments.length - 1]);
+            } else {
+                name = this.getClass().getName();
+            }
+        }
 
-		return name;
-	}
+        return name;
+    }
 
-	/**
-	 * This class name, even if instantiated as an anonymous class.
-	 *
-	 * @return a root logger name.
-	 */
-	private String getClazzName() {
-		final String delimeter = ".";
-		final String regEx = "\\.";
+    /**
+     * This class name, even if instantiated as an anonymous class.
+     *
+     * @return a root logger name.
+     */
+    private String getClazzName() {
+        final String delimeter = ".";
+        final String regEx = "\\.";
 
-		String name = null;
+        String name = null;
 
-		if (this.getClass().getSimpleName() != null && this.getClass().getSimpleName().length() != 0) {
-			name = this.getClass().getSimpleName();
-		} else {
-			if (this.getClass().getName().contains(delimeter)) {
-				final String nameSegments[] = this.getClass().getName().split(regEx);
+        if (this.getClass().getSimpleName() != null && this.getClass().getSimpleName().length() != 0) {
+            name = this.getClass().getSimpleName();
+        } else {
+            if (this.getClass().getName().contains(delimeter)) {
+                final String nameSegments[] = this.getClass().getName().split(regEx);
 
-				name = nameSegments[nameSegments.length - 1];
-			} else {
-				name = this.getClass().getName();
-			}
-		}
+                name = nameSegments[nameSegments.length - 1];
+            } else {
+                name = this.getClass().getName();
+            }
+        }
 
-		return name;
-	}
+        return name;
+    }
 
-	/**
-	 * Convert the content of the stream to a UTF-8 character set String.
-	 */
-	@Override
-	public String toString() {
-		String returnValue = null;
+    /**
+     * Convert the content of the stream to a UTF-8 character set String.
+     */
+    @Override
+    public String toString() {
+        String returnValue = null;
 
-		try {
-			returnValue = getOutput().toString("UTF-8");
-		} catch (UnsupportedEncodingException e) {
-			getLogger().error(e.getMessage(), e);
-		}
+        try {
+            returnValue = getOutput().toString("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            getLogger().error(e.getMessage(), e);
+        }
 
-		return returnValue;
-	}
+        return returnValue;
+    }
 }
