5dc61a2fc5866a19a40e0486bf6548bd3a3a183b
John McDonnell <john_mcdonnell@hotmail.com>
2016-03-16 12:40:14 +0000

    Refactoring due to a Class name clash with Apache Commons IO.

---
+++ MockTCPServer/src/main/java/io/cloudracer/datastream/DataStreamRegexMatcher.java
@@ -0,0 +1,28 @@
+package io.cloudracer.datastream;
+
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+
+public class DataStreamRegexMatcher extends TypeSafeMatcher<DataStream> {
+
+    private final String regex;
+
+    public DataStreamRegexMatcher(final String regex) {
+        this.regex = regex;
+    }
+
+    @Override
+    public void describeTo(final Description description) {
+        description.appendText("matches regular expression=`" + regex + "`");
+    }
+
+    @Override
+    public boolean matchesSafely(final DataStream data) {
+        return data.toString().matches(regex);
+    }
+
+    // matcher method you can call on this matcher class
+    public static DataStreamRegexMatcher matchesRegex(final String regex) {
+        return new DataStreamRegexMatcher(regex);
+    }
+}
