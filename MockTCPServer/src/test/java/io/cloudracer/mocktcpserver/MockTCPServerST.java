7ad1ce9637c22157e46704bd99eb8e3fbb7d6f6a
John McDonnell <john_mcdonnell@hotmail.com>
2016-03-16 11:41:20 +0000

    Simplification, refactoring and formatting.

--- MockTCPServer/src/test/java/io/cloudracer/mocktcpserver/MockTCPServerST.java
+++ MockTCPServer/src/test/java/io/cloudracer/mocktcpserver/MockTCPServerST.java
@@ -13,34 +13,34 @@ import io.cloudracer.tcpclient.TCPClient;
 
 public class MockTCPServerST {
 
-	private io.cloudracer.tcpclient.TCPClient client;
-	private MockTCPServer server;
+    private io.cloudracer.tcpclient.TCPClient client;
+    private MockTCPServer server;
 
-	@Before
-	public void setUp() throws Exception {
-		server = new MockTCPServer(TestConstants.MOCK_SERVER_PORT);
-		client = new TCPClient(TestConstants.MOCK_SERVER_PORT);
-	}
+    @Before
+    public void setUp() throws Exception {
+        server = new MockTCPServer(TestConstants.MOCK_SERVER_PORT);
+        client = new TCPClient(TestConstants.MOCK_SERVER_PORT);
+    }
 
-	@After
-	public void tearDown() throws Exception {
-		client.close();
-		server.close();
-	}
+    @After
+    public void tearDown() throws Exception {
+        client.close();
+        server.close();
+    }
 
-	@Test
-	public void mockTCPServerACK() throws ClassNotFoundException, IOException {
-		final String message = String.format("%s%s", "Test message!!", TestConstants.DEFAULT_TERMINATOR);
+    @Test
+    public void mockTCPServerACK() throws ClassNotFoundException, IOException {
+        final String message = String.format("%s%s", "Test message!!", TestConstants.DEFAULT_TERMINATOR);
 
-		assertEquals("Unexpected server response.", TestConstants.ACK, client.send(message).toString());
-	}
+        assertEquals("Unexpected server response.", TestConstants.ACK, client.send(message).toString());
+    }
 
-	@Test
-	public void mockTCPServerACKWithCustomTerminator() throws ClassNotFoundException, IOException {
-		final String message = String.format("%s%s", "Test message!!", TestConstants.CUSTOM_TERMINATOR);
+    @Test
+    public void mockTCPServerACKWithCustomTerminator() throws ClassNotFoundException, IOException {
+        final String message = String.format("%s%s", "Test message!!", TestConstants.CUSTOM_TERMINATOR);
 
-		server.setTerminator(TestConstants.CUSTOM_TERMINATOR.getBytes());
+        server.setTerminator(TestConstants.CUSTOM_TERMINATOR.getBytes());
 
-		assertEquals("Unexpected server response.", TestConstants.ACK, client.send(message).toString());
-	}
+        assertEquals("Unexpected server response.", TestConstants.ACK, client.send(message).toString());
+    }
 }
