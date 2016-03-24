7e9af66243c3dc5a7921825099713ae9a740d085
John McDonnell <john_mcdonnell@hotmail.com>
2016-03-24 15:52:07 +0000

    Documentation.

---
+++ MockTCPServer/src/main/java/io/cloudracer/mocktcpserver/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * MockTCPServer simulates a TCP Server in environments where the *real* server is not available. The *real* TCP Server may still be in development but you need to continue developing the integrations with other systems. Perhaps installing and configuring one or more Enterprise Application in a System Test environment is prohibitively time consuming and/or costly.
+ * <p>
+ * MockTCPServer has a number of features specifically designed for Integration/System testing. No particular Test Framework is preferred or required.
+ * <p>
+ * Source code is made available, under an Open Source <a href="https://github.com/CloudRacer/MockTCPServer/blob/master/LICENSE.md" target="_blank">licence</a>, on <a href="https://github.com/CloudRacer/MockTCPServer" target="_blank">GitHub</a>. <a href="http://mocktcpserver.cloudracer.io/docs/api/" target="_blank">Javadocs</a> are currently very limited and quite sparse but they will be improved with each release.
+ * <p>
+ * More information is available on the <a href="http://mocktcpserver.cloudracer.io/">MockTCPServer</a> home page.
+ *
+ * @author John McDonnell
+ **/
+package io.cloudracer.mocktcpserver;
