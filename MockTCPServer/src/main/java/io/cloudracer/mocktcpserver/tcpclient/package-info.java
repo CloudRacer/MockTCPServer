f9eda8ab3b48ea8de4603e4b980005d0893ac37b
John McDonnell <jmcdonnell3@csc.com>
2016-03-26 16:23:14 +0000

    Documentation.

---
+++ MockTCPServer/src/main/java/io/cloudracer/mocktcpserver/tcpclient/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * TCPClient is provided mainly to facilitate demonstration of the {@link io.cloudracer.mocktcpserver.MockTCPServer MockTCPServer}, and for use in test suites.
+ * <p>
+ * The TCPClient sends a message to a specified Machine, on a specified port, and waits for a synchronous response. This behavior, and <b>much more is configurable</b> so that the TCPClient can be used in as wide a variety of test suites as possible.
+ * 
+ * @author John McDonnell
+ **/
+package io.cloudracer.mocktcpserver.tcpclient;
