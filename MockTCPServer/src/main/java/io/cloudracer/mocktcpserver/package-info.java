e0fe04a9e1be5fc17f795b0d35ac2d833ad35409
John McDonnell <john_mcdonnell@hotmail.com>
2016-03-31 11:50:10 +0100

    Documentation.

--- MockTCPServer/src/main/java/io/cloudracer/mocktcpserver/package-info.java
+++ MockTCPServer/src/main/java/io/cloudracer/mocktcpserver/package-info.java
@@ -1,9 +1,9 @@
 /**
- * MockTCPServer simulates a TCP Server in environments where the <b>real</b> server is not available. The <b>real</b> TCP Server may still be in development but you need to continue developing the integrations with other systems. Perhaps installing and configuring one or more Enterprise Application in a System Test environment is prohibitively time consuming and/or costly.
+ * MockTCPServer simulates a TCP Server in environments where the <b>real</b> server is not available. The <b>real</b> TCP Server may still be in development but you need to continue developing the integrations with other systems. Perhaps installing and configuring one or more Enterprise Applications in a System Test environment is prohibitively time consuming and/or costly.
  * <p>
  * MockTCPServer has a number of features specifically designed for Integration/System testing. No particular Test Framework is preferred or required.
  * <p>
- * Source code is made available, under an Open Source <a href="https://github.com/CloudRacer/MockTCPServer/blob/master/LICENSE.md" target="_blank">licence</a>, on <a href="https://github.com/CloudRacer/MockTCPServer" target="_blank">GitHub</a>. <a href="http://mocktcpserver.cloudracer.io/docs/api/" target="_blank">Javadocs</a> are currently very limited and quite sparse but they will be improved with each release.
+ * Source code is made available, under an Open Source <a href="https://github.com/CloudRacer/MockTCPServer/blob/master/LICENSE.md">licence</a>, on <a href="https://github.com/CloudRacer/MockTCPServer">GitHub</a>. <a href="http://mocktcpserver.cloudracer.io/docs/api/">Javadocs</a> are available for the latest version and each previous release.
  * <p>
  * More information is available on the <a href="http://mocktcpserver.cloudracer.io/">MockTCPServer</a> home page.
  *
