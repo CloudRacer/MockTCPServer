9299752938061695315feb76ceef2d38fa55db56
John McDonnell <john_mcdonnell@hotmail.com>
2016-04-01 17:06:00 +0100

    Refactoring.

--- MockTCPServer/src/test/java/io/cloudracer/TestRapidMessageThroughput.java
+++ MockTCPServer/src/test/java/io/cloudracer/TestRapidMessageThroughput.java
@@ -1,6 +1,6 @@
 package io.cloudracer;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
 import java.io.IOException;
 
@@ -9,42 +9,40 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Send many messages in a tight loop to ensure that "maximum" volume throughput
- * is possible.
+ * Send many messages in a tight loop to ensure that "maximum" volume throughput is possible.
  *
  * @author John McDonnell
  */
 public class TestRapidMessageThroughput extends AbstractTestTools {
 
-	@Override
-	@Before
-	public void setUp() throws IOException {
-		super.setUp();
-	}
+    @Override
+    @Before
+    public void setUp() throws IOException {
+        super.setUp();
+    }
 
-	@Override
-	@After
-	public void cleanUp() throws IOException {
-		super.cleanUp();
-	}
+    @Override
+    @After
+    public void cleanUp() throws IOException {
+        super.cleanUp();
+    }
 
-	/**
-	 * Send many messages in a tight loop to ensure that "maximum" volume
-	 * throughput is possible.
-	 *
-	 * @throws IOException
-	 * @throws ClassNotFoundException
-	 **/
-	@Test(timeout = TestConstants.TEST_TIMEOUT_10_MINUTE)
-	public void rapidMessageThroughput() throws ClassNotFoundException, IOException {
-		final int totalServerRestarts = 1000;
-		for (int i = 0; i < totalServerRestarts; i++) {
-			final String message = String.format("Test %d%s", i, TestConstants.DEFAULT_TERMINATOR);
+    /**
+     * Send many messages in a tight loop to ensure that "maximum" volume throughput is possible.
+     *
+     * @throws IOException
+     * @throws ClassNotFoundException
+     **/
+    @Test(timeout = TestConstants.TEST_TIMEOUT_10_MINUTE)
+    public void rapidMessageThroughput() throws ClassNotFoundException, IOException {
+        final int totalServerRestarts = 1000;
+        for (int i = 0; i < totalServerRestarts; i++) {
+            final String message = String.format("Test %d%s", i, TestConstants.DEFAULT_TERMINATOR);
 
-			assertEquals("Unexpected server response.", TestConstants.ACK, this.getClient().send(message).toString());
-		}
+            assertArrayEquals("Unexpected server response.", TestConstants.ACK, this.getClient().send(message).toByteArray());
+        }
 
-		this.checkLogMonitorForUnexpectedMessages();
-	}
+        this.checkLogMonitorForUnexpectedMessages();
+    }
 
 }
